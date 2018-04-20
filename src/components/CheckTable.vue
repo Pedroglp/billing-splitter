7ebf457bab09630203411d001fda1f790925ab63
pedroglp <pedrogabriellancelloti@poli.ufrj.br>
2018-04-20 15:57:08 -0300

    new build

--- src/components/CheckTable.vue
+++ src/components/CheckTable.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="checktable">
         <h1>{{`Mesa ${$route.params.id}`}}</h1>
-        <input type="text" placeholder="Nome do Cliente">
         <h2>Pedidos da Mesa</h2>
         <productlist :items="getProducts"></productlist>
         <h2>Clientes</h2>
@@ -23,11 +22,11 @@
             }
         },
         computed:{
-            getTable(){
+            getTable() {
                 let table = this.$store.getters.getTable(parseInt(this.$route.params.id))
                 return table
             },
-            getProducts(){
+            getProducts() {
                 return this.$store.getters.getTableProducts(parseInt(this.$route.params.id))
             }
         },
