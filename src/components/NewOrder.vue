36eb65fdf24d22c41d783eb1ae7814bd655de05b
pedroglp <pedrogabriellancelloti@poli.ufrj.br>
2018-04-20 09:34:14 -0300

    Adicionando Front para incluir pedido na mesa

--- src/components/NewOrder.vue
+++ src/components/NewOrder.vue
@@ -1,26 +1,53 @@
 <template>
     <div class="neworder">
-        <h1>Pedido</h1>
-        <input type="text" value="Nome do Cliente">
-        <orderlist :items="getProducts"></orderlist>
-        <button>Adicionar</button>
+        <h1>Novo Pedido</h1>
+        <input type="text" placeholder="Nome do Cliente">
+        <h2>Cardápio</h2>
+        <productlist :items="getProducts"></productlist>
+        <h2>Escolhidos</h2>
+        <orderlist :items="getOrderFromCostumer"></orderlist>
+        <button @click="saveTableOrder">Finalizar</button>
+        <button @click="saveCostumerOrder">Adicionar Pedido</button>
     </div>
 </template>
 
 <script>
+    import productlist from "./NewOrder/ProductList.vue"
     import orderlist from "./Common/OrderList.vue"
+    import router from '../router'
 
     export default {
         name:'neworder',
-        components:{orderlist},
-        data(){
+        components:{productlist, orderlist},
+        data() {
             return {
+                tableId:1,
+                costumerId: 1,
+                costumerName: '',
+                costumersAtTable: []
             }
         },
-        computed:{
+        computed: {
             getProducts(){
                 return this.$store.getters.getProductList
+            },
+            getOrderFromCostumer(){
+                let costumer = this.$store.getters.getCostumer({tableId: this.tableId, costumerId: this.costumerId})
+                return costumer.products
             }
+        },
+        methods: {
+            saveCotumerOrder(){
+                //commit addCostumerToTable
+                this.costumerId += 1
+                this.costumerName = ''
+            },
+            saveTableOrder(){
+                router.go(-1)
+            },
+        },
+        created() {
+            //commit pushTable
         }
     }
 </script>
