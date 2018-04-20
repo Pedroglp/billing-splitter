37fff9b0fc09956e8c9876bbb034fa0d902bcd4b
pedroglp <pedrogabriellancelloti@poli.ufrj.br>
2018-04-20 09:40:17 -0300

    Adicionando Listagem de Produtos Disponíveis

---
+++ src/components/NewOrder/ProductList.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="product-list small-table">
+        <table>
+            <th></th>
+            <tr v-for="item in items" :key="item.id">
+                <td><a>{{item.name}}</a></td>
+                <td>{{`R$ ${item.value}`}}</td>
+                <td><button>+</button></td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'productlist',
+        props: ['items'],
+        data(){
+            return {}
+        },
+        methods:{
+            onClickItem(){ alert('Item Clicked') },
+        }
+    }
+</script>
+
+<style>
+
+table{
+    text-align: center;
+    width: 100%;
+    height: 25px;
+    overflow: scroll;
+    margin-bottom: 5vh;
+}
+
+</style>
