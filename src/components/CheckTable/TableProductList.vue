e48d9b36d78f425770fa187e65b8a63e63167979
pedroglp <pedrogabriellancelloti@poli.ufrj.br>
2018-04-20 09:39:39 -0300

    Adicionando Listagem de Produtos na Mesa

---
+++ src/components/CheckTable/TableProductList.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="product-list">
+        <div class="small-table">
+        <table>
+            <th></th>
+            <tr v-for="item in items" :key="item.id">
+                <td><a>{{item.name}}</a></td>
+                <td>{{`R$ ${item.value}`}}</td>
+            </tr>
+        </table>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'productlist',
+        props: ['items'],
+        data(){
+            return {}
+        },
+    }
+</script>
