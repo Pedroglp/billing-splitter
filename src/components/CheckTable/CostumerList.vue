5c474e3544c15aa008496525fcbd8c8161fa0b1e
pedroglp <pedrogabriellancelloti@poli.ufrj.br>
2018-04-20 09:38:43 -0300

    Adicionando componentes de listagem de clientes

---
+++ src/components/CheckTable/CostumerList.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="costumer-list small-list">
+        <table>
+            <th></th>
+            <tr v-for="costumer in table.costumers" :key="costumer.id">
+                <td><a @click="onClickCostumer(costumer.id)">{{costumer.name}}</a></td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+    import router from '../../router'
+
+    export default {
+        name:'costumerlist',
+        props: ['table'],
+        data(){
+            return {
+                costumers: this.table.costumers
+            }
+        },
+        methods:{
+            onClickCostumer(id){ router.push(`/cliente/${id}?tableId=${this.table.id}`) },
+        }
+    }
+</script>
+
+<style>
