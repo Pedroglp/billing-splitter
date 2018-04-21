78f406e0330311a57b1cbe16edcbb1dfaa6cb62a
pedroglp <pedrogabriellancelloti@poli.ufrj.br>
2018-04-21 10:37:02 -0300

    fix path

--- src/router/index.js
+++ src/router/index.js
@@ -8,10 +8,10 @@ import CheckCostumer from '../components/CheckCostumer.vue'
 Vue.use(VueRouter)
 
 const routes = [
-  { path: '/', name: 'Tables', component: Tables },
-  { path: '/novo-pedido', name: 'NewOrder', component: NewOrder},
-  { path: '/mesa/:id', name: 'CheckTable', component: CheckTable},
-  { path: '/cliente/:id', name: 'CheckCostumer', component: CheckCostumer}
+  { path: '/billing-splitter/', name: 'Tables', component: Tables },
+  { path: '/billing-splitter/novo-pedido', name: 'NewOrder', component: NewOrder},
+  { path: '/billing-splitter/mesa/:id', name: 'CheckTable', component: CheckTable},
+  { path: '/billing-splitter/cliente/:id', name: 'CheckCostumer', component: CheckCostumer}
 ]
 
 export default new VueRouter({
